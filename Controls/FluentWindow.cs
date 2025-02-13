197aefcb00af4378056f5e72f231308ff022982d
vddCore <vddcore@outlook.com>
2025-02-13 02:47:17 +0100

    even more workies.

--- Controls/FluentWindow.cs
+++ Controls/FluentWindow.cs
@@ -7,8 +7,8 @@ using Avalonia.Controls.Chrome;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Interactivity;
-using Avalonia.Layout;
 using Avalonia.Media;
+using Avalonia.Media.Imaging;
 using Avalonia.Platform;
 
 public class FluentWindow : Window
@@ -23,6 +23,9 @@ public class FluentWindow : Window
     private CompositeDisposable _disposables;
 
     protected override Type StyleKeyOverride => typeof(FluentWindow);
+
+    public new static readonly StyledProperty<Bitmap> IconProperty =
+        AvaloniaProperty.Register<FluentWindow, Bitmap>(nameof(Icon));
 
     public static readonly StyledProperty<bool> ShowMinimizeButtonProperty =
         AvaloniaProperty.Register<FluentWindow, bool>(
@@ -58,6 +61,16 @@ public class FluentWindow : Window
         AvaloniaProperty.Register<FluentWindow, Thickness>(
             nameof(CaptionContentSafeAreaMargin)
         );
+
+    public new Bitmap Icon
+    {
+        get => GetValue(IconProperty);
+        set
+        {
+            SetValue(IconProperty, value);
+            base.Icon = new WindowIcon(Icon);
+        }
+    }
 
     public bool ShowMaximizeButton
     {
@@ -155,7 +168,7 @@ public class FluentWindow : Window
     {
         base.OnApplyTemplate(e);
 
-        _captionBar = e.NameScope.Find<Grid>("PART_CaptionBar");
+        _captionBar = e.NameScope.Find<Control>("PART_CaptionBar");
 
         if (_captionBar == null)
         {
@@ -173,6 +186,7 @@ public class FluentWindow : Window
 
         if (captionButtons != null)
         {
+            captionButtons.Resources.Add("CaptionButtonHeight", CaptionBarHeight);
             captionButtons.TemplateApplied += CaptionButtons_TemplateApplied;
             captionButtons.LayoutUpdated += CaptionButtons_LayoutUpdated;
             captionButtons.Attach(this);
@@ -195,7 +209,7 @@ public class FluentWindow : Window
         if (_closeButton != null)
             _closeButton.IsVisible = ShowCloseButton;
     }
-    
+
     private void CaptionButtons_LayoutUpdated(object? sender, EventArgs e)
     {
         CaptionContentSafeAreaMargin = new Thickness(0, 0, ((CaptionButtons)sender!).Bounds.Width, 0);
